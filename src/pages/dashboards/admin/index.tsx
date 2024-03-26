6574f28606959d6b88bdaee2c4b233b0f5455049
Kike <josellique@gmail.com>
2024-03-25 21:45:44 -0600

    Fix admin building  error

--- src/pages/dashboards/admin/index.tsx
+++ src/pages/dashboards/admin/index.tsx
@@ -4,9 +4,7 @@ import PageHeader from './PageHeader';
 import { Container, Grid, ThemeProvider } from '@mui/material';
 
 
-import AccountBalance from './AccountBalance';
-import Wallets from './Wallets';
-import WatchList from './WatchList';
+
 import { PureLightTheme } from '~/styles/schemes/PureLightTheme';
 import PageTitleWrapper from '~/components/PageTitleWrapper';
 import SidebarLayout from '~/layouts/SidebarLayout/';
@@ -32,17 +30,15 @@ function DashboardAdmin() {
               alignItems={"stretch"}
               spacing={4}>
               <Grid item xs={12}>
-                <AccountBalance />
+               
               </Grid>
               <Grid item lg={8} xs={12}>
-                <Wallets />
+                
               </Grid>
-              {/* <Grid item lg={4} xs={12}>
-                <AccountSecurity/>
-              </Grid> */}
+             
             </Grid>
             <Grid item xs={12}>
-              <WatchList />
+              
             </Grid>
           </Container>
           
